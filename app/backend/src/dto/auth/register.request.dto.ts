b802a1a7532260626b4ef8c14a58e55c4dc13ef2
hyzoon <shj83811081@gmail.com>
2025-04-12 06:01:40 +0900

    refactor: redesign auth endpoints
    - register only create user row
    - adapt changes in db schema
    - update dtos

--- app/backend/src/dto/auth/register.request.dto.ts
+++ app/backend/src/dto/auth/register.request.dto.ts
@@ -1,53 +1,8 @@
-import { Gender, MBTI } from '@prisma/client';
-import { Type } from 'class-transformer';
-import {
-  IsEmail,
-  IsEnum,
-  IsJSON,
-  IsNumber,
-  IsObject,
-  IsOptional,
-  IsString,
-  Max,
-  MaxLength,
-  Min,
-  MinLength,
-  ValidateNested,
-} from 'class-validator';
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
-class PersonalityDto {
-  @IsNumber()
-  @Min(0)
-  @Max(5)
-  openness: number;
-
-  @IsNumber()
-  @Min(0)
-  @Max(5)
-  conscientiousness: number;
-
-  @IsNumber()
-  @Min(0)
-  @Max(5)
-  extraversion: number;
-
-  @IsNumber()
-  @Min(0)
-  @Max(5)
-  agreeableness: number;
-
-  @IsNumber()
-  @Min(0)
-  @Max(5)
-  neuroticism: number;
-}
-
-// TODO: 길이 제한은 논의 후 수정 필요
 export class RegisterRequestDto {
-  @IsString()
-  @MinLength(5)
-  @MaxLength(15)
-  username: string;
+  @IsEmail()
+  email: string;
 
   @IsString()
   name: string;
@@ -56,36 +11,4 @@ export class RegisterRequestDto {
   @MinLength(8)
   @MaxLength(20)
   password: string;
-
-  @IsEmail()
-  email: string;
-
-  @IsString()
-  @IsOptional()
-  introduce?: string;
-
-  @IsNumber()
-  age: number;
-
-  @IsEnum(Gender)
-  gender: Gender;
-
-  @IsEnum(MBTI)
-  @IsOptional()
-  mbti?: MBTI;
-
-  @IsJSON()
-  @IsOptional()
-  interests?: string[]; // TODO: 타입 정의 필요?
-
-  @IsString()
-  province: string;
-
-  @IsString()
-  city: string;
-
-  @IsObject()
-  @ValidateNested()
-  @Type(() => PersonalityDto)
-  personality: PersonalityDto;
 }
